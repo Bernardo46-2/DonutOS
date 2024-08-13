9eaa20da5e645a9225e6584402456728adfad944
Bernardo <103369540+Bernardo46-2@users.noreply.github.com>
2024-08-12 22:24:54 -0300

    Update donut.c

--- kernel/src/donut.c
+++ kernel/src/donut.c
@@ -1,3 +1,29 @@
+/* 
+ * Donut 3D - Código original © 2023 Andy Sloane, licenciado sob a licença MIT.
+ * Modificações significativas feitas por [Seu Nome] em [Data].
+ * Modificações incluem: [Descrição das Modificações].
+ *
+ * Copyright (c) 2023 Andy Sloane
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "../include/donut.h"
 #include "../include/tty.h"
 #include "../include/kb.h"
